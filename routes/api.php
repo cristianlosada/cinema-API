7ce14c245ee69e01a1827fb7969639e5279c3ff3
Crissoft <cristianlosano314@gmail.com>
2024-05-08 21:13:13 -0500

    agrega enrutamiento

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MoviesController;
 
 // forma de acceder a las rutas agrupadas del proyecto
+// Route::controller(MoviesController::class)->group(function () {
+//     Route::get('/movies',            'index');
+//     Route::get('/movie/{id}',        'show');
+//     Route::post('/movie',            'store');
+//     // Route::get('/movie/{id}/tareas', 'tareas');
+//     // Route::post('/moviefilter',      'filter');
+//     // Route::put('/movie/{id}',        'edit');
+//     // Route::delete('/movie/{id}',     'delete');
+
+// })->middleware('sanctum');
+
+// forma de acceder a las rutas agrupadas del proyecto
 Route::controller(MoviesController::class)->group(function () {
-    Route::get('/movies',            'index');
-    Route::get('/movie/{id}',        'show');
-    Route::post('/movie',            'store');
-    // Route::get('/movie/{id}/tareas', 'tareas');
-    // Route::post('/moviefilter',      'filter');
-    // Route::put('/movie/{id}',        'edit');
-    // Route::delete('/movie/{id}',     'delete');
+    Route::get('/listar-generos',    'obtenerGeneros');
+    Route::get('/listar-peliculas',  'obtenerPeliculas');
 })->middleware('sanctum');
